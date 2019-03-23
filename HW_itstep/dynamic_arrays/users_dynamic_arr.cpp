c00cc87808b1668215892a997ddcad0e1811ce89
Marbax <marbax117@gmail.com>
2019-03-23 19:25:10 +0200

    Started dinamyc arrays hw

---
+++ HW_itstep/dynamic_arrays/users_dynamic_arr.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+// 1. Создать динамический массив, размер которого пользователь вводит с
+// клавиатуры. Заполнить его случайными числами из диапазона [10;20] и вывести
+// его на экран.
+
+void Fill_array(int *arr, int size)
+{
+    int max = 20, min = 10;
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = rand() % (max - min) + min;
+    }
+}
+
+void Print_array(int *arr, int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    srand(time(0));
+
+    int size = 0;
+    cout << "Enter array's size" << endl;
+    cin >> size;
+    int *arr = new int[size];
+    Fill_array(arr, size);
+    Print_array(arr, size);
+    delete[] arr;
+}
