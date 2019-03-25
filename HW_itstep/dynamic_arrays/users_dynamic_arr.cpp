81d624e9baf52b03b2cea07a98331a04978c4e83
Nikita Morozov <marbax117@gmail.com>
2019-03-26 00:14:49 +0200

    Hw added

--- HW_itstep/dynamic_arrays/users_dynamic_arr.cpp
+++ HW_itstep/dynamic_arrays/users_dynamic_arr.cpp
@@ -7,6 +7,7 @@ using namespace std;
 
 void Fill_array(int *arr, int size)
 {
+    srand(time(0));
     int max = 20, min = 10;
     for (int i = 0; i < size; i++)
     {
@@ -25,8 +26,6 @@ void Print_array(int *arr, int size)
 
 int main()
 {
-    srand(time(0));
-
     int size = 0;
     cout << "Enter array's size" << endl;
     cin >> size;
--- HW_itstep/if-else-ternarn/Hw-if-else-1-3.cpp
+++ HW_itstep/if-else-ternarn/Hw-if-else-1-3.cpp
@@ -1,17 +1,19 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 int main()
 {
     float num;
-    int sec , b_dot , a_dot;
-    cout << "Welcome to a program that will translate time into seconds.\n\nEnter the time(minutes.seconds) : " ;
+    int sec, b_dot, a_dot;
+    cout << "Welcome to a program that will translate time into seconds.\n\nEnter the time(minutes.seconds) : ";
     cin >> num;
-    a_dot = (float(num)-(int)num)*100;
-    if (a_dot<60) {
+    a_dot = (float(num) - (int)num) * 100;
+    if (a_dot < 60)
+    {
         b_dot = (int)num * 60;
         sec = a_dot + b_dot;
         cout << "There are " << sec << " seconds";
     }
-    else cout << "\nError. Amount of seconds can't be more than 60";
+    else
+        cout << "\nError. Amount of seconds can't be more than 60";
 }
