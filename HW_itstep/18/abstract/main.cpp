c35755cc298c7fc004eb50e53fe19af4a8bf7944
Marbax <marbax117@gmail.com>
2019-09-03 10:18:01 +0300

    hw18 started

---
+++ HW_itstep/18/abstract/main.cpp
@@ -0,0 +1,11 @@
+/* Создать абстрактный базовый класс Automobile с полем «number» и
+чисто виртуальным деструктором. От класса Automobile наследуется два
+абстрактных класса: Comm on(поле «color») и Special(поле
+«organization»). От Common наследуются три класса: Sedan, Minivan,
+Pickup. От класса Special наследуются абстрактный класс Builder с чисто
+виртуальной функцией work и абстрактный класс Emergency c чисто
+виртуальной функцией signal. От класса Builder наследуются три класса:
+TruckCrane, ConcreteMixer, Tipper, переопределяющие метод work. От
+класса Emergency наследуются три класса: Police, Ambulance, FireEngine,
+переопределяющие метод signal. */
+
