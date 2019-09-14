b7e253dc94cb6efc72cb6c8aec35cd4936e41ba9
Marbax <marbax117@gmail.com>
2019-09-14 21:27:16 +0300

    hw19 stupid bridge realization completed

--- HW_itstep/19/struct_pattern/main.cpp
+++ HW_itstep/19/struct_pattern/main.cpp
@@ -2,138 +2,151 @@
 предмета, каждый из которых может быть двух видов: лекция или практика.
 2. Создать UML диаграмму классов для приложения. */
 
-// BRIDGE
 
 #include <iostream>
-#include <string>
 using namespace std;
 
-class IWallCreator
-{
-public:
-    virtual void BuildWall() = 0;
-    virtual void BuildWallWithDoor() = 0;
-    virtual void BuildWallWithWindow() = 0;
-    virtual ~IWallCreator() = default;
-};
+//-------------------------------------------------------------------------------------
+//---------------------------------------Предмет---------------------------------------
+//-------------------------------------------------------------------------------------
 
-class BrickWallCreator : public IWallCreator
+class AbstractLesson
 {
-public:
-    void BuildWall() override
-    {
-        cout << "Creating brick wall" << endl;
-    }
-
-    void BuildWallWithDoor() override
-    {
-        cout << "Creating brick wall with door" << endl;
-    }
-    void BuildWallWithWindow() override
-    {
-        cout << "Creating brick wall with window" << endl;
-    }
-};
-
-class ConcreteWallCreator : public IWallCreator
-{
-public:
-    void BuildWall() override
-    {
-        cout << "Creating concrete wall" << endl;
-    }
-    void BuildWallWithDoor() override
-    {
-        cout << "Creating concrete wall with door" << endl;
-    }
-    void BuildWallWithWindow() override
-    {
-        cout << "Creating concrete wall with window" << endl;
-    }
-};
-
-class IBuldingCompany
-{
-    IWallCreator *WallCreator;
+private:
+    string teacher = "UNKNOWN";
+    string lesson_place = "UNKNOWN";
+    string lesson_name = "UNKNOWN";
 
 public:
-    virtual void BuildFoundation() = 0;
-    virtual void BuildRoom() = 0;
-    virtual void BuildRoof() = 0;
-    virtual ~IBuldingCompany() = default;
-    IWallCreator *getWallCreator() const
-    {
-        return WallCreator;
-    }
+    AbstractLesson() = default;
+    AbstractLesson(string teacher) : teacher(teacher) {}
+    AbstractLesson(string teacher, string lesson_place) : AbstractLesson(teacher) { this->lesson_place = lesson_place; }
 
-    void setWallCreator(IWallCreator *WallCreator)
-    {
-        IBuldingCompany::WallCreator = WallCreator;
-    }
+    virtual void setTeacher(string teacher) { this->teacher = teacher; }
+
+    virtual void setLessonPlace(string lesson_place) { this->lesson_place = lesson_place; }
+
+    virtual void setLessonName(string lesson_name) { this->lesson_name = lesson_name; }
+
+    virtual string getTeacher() const { return teacher; }
+
+    virtual string getLessonPlace() const { return lesson_place; }
+
+    virtual string getLessonName() const { return lesson_name; }
+
+    virtual ~AbstractLesson() = 0;
+};
+AbstractLesson::~AbstractLesson() { cout << "Hope that was last lesson today..." << endl; }
+
+class CyberSecurityLesson : public AbstractLesson
+{
+
+public:
+    CyberSecurityLesson() : AbstractLesson("Cybertron 3000", "426") { this->setLessonName("cybersecurity"); }
+    /* some classy realization */
+    ~CyberSecurityLesson() = default;
 };
 
-class BuldingCompany : public IBuldingCompany
+class DesigningLesson : public AbstractLesson
 {
 public:
-    void BuildFoundation() override
-    {
-        cout << "Foundation is built." << endl;
-    }
-
-    void BuildRoom() override
-    {
-        getWallCreator()->BuildWallWithDoor();
-        getWallCreator()->BuildWall();
-        getWallCreator()->BuildWallWithWindow();
-        getWallCreator()->BuildWall();
-        cout << "Room finished." << endl;
-    }
-
-    void BuildRoof() override
-    {
-        cout << "Roof is done." << endl;
-    }
+    DesigningLesson() : AbstractLesson("Designerbot 696", "766") { this->setLessonName("designing"); }
+    /* some classy realization */
+    ~DesigningLesson() = default;
 };
 
-class NearSeaBuldingCompany : public IBuldingCompany
+class ProgrammingLesson : public AbstractLesson
 {
 public:
-    void BuildFoundation() override
+    ProgrammingLesson() : AbstractLesson("Programmator the void", "3/4") { this->setLessonName("programming"); }
+    /* some classy realization */
+    ~ProgrammingLesson() = default;
+};
+
+//-------------------------------------------------------------------------------------
+//---------------------------------------Вид_предмета----------------------------------
+//-------------------------------------------------------------------------------------
+
+class AbstractLessonType
+{
+    AbstractLesson *Lesson;
+
+public:
+    virtual void visitLesson() = 0;
+    virtual void skipLesson() = 0;
+    virtual void editLessonParams() { cout << "Permission denied" << endl; }
+    virtual void listenToTeacher() = 0;
+
+    AbstractLesson *getLesson() const { return Lesson; }
+
+    void setLesson(AbstractLesson *Lesson) { this->Lesson = Lesson; }
+
+    virtual ~AbstractLessonType() = default;
+};
+
+class TheoreticLesson : public AbstractLessonType
+{
+    void listenToTeacher() override
     {
-        cout << "Foundation near sea is built." << endl;
+        cout << getLesson()->getTeacher() + " said \"bla-bla\", but you aren't understand " + getLesson()->getLessonName() + " language" << endl;
     }
 
-    void BuildRoom() override
-    {
-        getWallCreator()->BuildWallWithDoor();
-        getWallCreator()->BuildWall();
-        getWallCreator()->BuildWallWithWindow();
-        getWallCreator()->BuildWall();
-        cout << "Room near sea finished." << endl;
-    }
+public:
+    void skipLesson() override { cout << "You will't attend on a theory of " + getLesson()->getLessonName() + " lesson" << endl; }
 
-    void BuildRoof() override
+    void visitLesson() override
     {
-        cout << "Roof near sea is done." << endl;
+        cout << "You will attend on a " + getLesson()->getLessonName() + " lesson" << endl;
+        this->listenToTeacher();
     }
 };
+
+class PraciceLesson : public AbstractLessonType
+{
+    void listenToTeacher() override
+    {
+        cout << getLesson()->getTeacher() + " make some " + getLesson()->getLessonName() + " things" << endl;
+        if (getLesson()->getLessonName() == "programming")
+        {
+            cout << getLesson()->getTeacher() + " gonna find, u can never hide " << endl;
+        }
+    }
+
+public:
+    void skipLesson() override { cout << "You will't attend on a practice of" + getLesson()->getLessonName() + " lesson" << endl; }
+
+    void visitLesson() override
+    {
+        cout << "You will attend on a " + getLesson()->getLessonName() + " lesson" << endl;
+        this->listenToTeacher();
+    }
+};
+//-------------------------------------------------------------------------------------
 
 int main()
 {
-    BrickWallCreator *bwc = new BrickWallCreator();
-    ConcreteWallCreator *cwc = new ConcreteWallCreator();
-    IBuldingCompany *bc = new BuldingCompany();
-    bc->setWallCreator(bwc);
-    bc->BuildFoundation();
-    bc->BuildRoom();
-    bc->BuildRoof();
-    delete bc;
-    bc = new NearSeaBuldingCompany();
-    bc->setWallCreator(cwc);
-    bc->BuildFoundation();
-    bc->BuildRoom();
-    bc->BuildRoof();
-    delete bc;
-    delete bwc;
-    delete cwc;
+    CyberSecurityLesson *cybles = new CyberSecurityLesson();
+    DesigningLesson *desles = new DesigningLesson();
+    ProgrammingLesson *progles = new ProgrammingLesson();
+    AbstractLessonType *lestype = new TheoreticLesson();
+
+    lestype->setLesson(cybles);
+    lestype->skipLesson();
+    lestype->setLesson(progles);
+    lestype->visitLesson();
+    delete lestype;
+
+    lestype = new PraciceLesson();
+    lestype->setLesson(desles);
+    lestype->visitLesson();
+    delete lestype;
+
+    lestype = new PraciceLesson();
+    lestype->setLesson(progles);
+    lestype->skipLesson();
+
+    delete lestype;
+    delete cybles;
+    delete desles;
+    delete progles;
 }
